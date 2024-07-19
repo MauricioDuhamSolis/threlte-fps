243e36e6e57275f144a3691f8ddb70c2b58d7507
MauricioDuhamSolis <mauricioduhamsolis@gmail.com>
2024-07-19 09:37:26 -0300

    remove soldier

--- src/lib/GameWorld.svelte
+++ src/lib/GameWorld.svelte
@@ -5,7 +5,7 @@
 	import { World } from '@threlte/rapier';
 	import GameSystems from './GameSystems.svelte';
 	import Helmet from './components/models/Helmet.svelte';
-	import Soldier from './components/models/Soldier.svelte';
+	// import Soldier from './components/models/Soldier.svelte';
 	import DebugGame from './debug/DebugGame.svelte';
 	import GunSystem from './gun/GunSystem.svelte';
 	import Ground from './scene/Ground.svelte';
@@ -24,7 +24,7 @@
 		<Player />
 		<GunSystem />
 		<Helmet />
-		<Soldier />
+		<!-- <Soldier /> -->
 
 		<!-- <AutoColliders>
 			<T.Mesh position.z={-25} position.y={2}>
